7ee5841ded53b2bd80c2f753911d7705d4d8b326
Pipotka <157719919+Pipotka@users.noreply.github.com>
2025-08-25 17:24:49 +0300

    Добавил инфраструктуру для тестов

---
+++ UnitTests/Extensions/TransactionRepositoryMockExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using Moq.Language.Flow;
+using Nasurino.SmartWallet.Context.Repository.Contracts;
+using Nasurino.SmartWallet.Entities;
+
+namespace Nasurino.SmartWallet.UnitTests.Extensions;
+
+/// <summary>
+/// Методы-расширения для моков <see cref="ITransactionRepository"/>
+/// </summary>
+public static class TransactionRepositoryMockExtensions
+{
+    /// <summary>
+    /// Настраивает мок для возврата списка транзакций.
+    /// <inheritdoc cref="ITransactionRepository.GetListByTimeRangeAndUserIdAsync" path="/summary"/>
+    /// </summary>
+    /// <param name="mockedTransactionRepository">Мок репозитория транзакций</param>
+    /// <param name="transactions">Список транзакций для возврата</param>
+    /// <param name="userId">ID пользователя</param>
+    /// <param name="startTimeRange">Начало временного диапазона</param>
+    /// <param name="endTimeRange">Конец временного диапазона</param>
+    /// <param name="token">Токен отмены</param>
+    /// <remarks>
+    /// <inheritdoc cref="ITransactionRepository.GetListByTimeRangeAndUserIdAsync" path="/remarks"/>
+    /// </remarks>
+    public static void GetListByTimeRangeReturnValue(this Mock<ITransactionRepository> mockedTransactionRepository,
+        IReadOnlyCollection<Transaction> transactions,
+        Guid userId = default,
+        DateTime startTimeRange = default,
+        DateTime endTimeRange = default,
+        CancellationToken token = default)
+        => mockedTransactionRepository.SetupListByTimeRange(userId, startTimeRange, endTimeRange, token)
+            .ReturnsAsync(transactions);
+
+    private static ISetup<ITransactionRepository, Task<IReadOnlyCollection<Transaction>>> SetupListByTimeRange(
+        this Mock<ITransactionRepository> mockedTransactionRepository,
+        Guid userId = default,
+        DateTime startTimeRange = default,
+        DateTime endTimeRange = default,
+        CancellationToken token = default)
+    {
+        return mockedTransactionRepository.Setup(x => x.GetListByTimeRangeAndUserIdAsync(
+            It.Is<Guid>(g => userId == Guid.Empty || g == userId),
+            It.Is<DateTime>(d => startTimeRange == default || d == startTimeRange),
+            It.Is<DateTime>(d => endTimeRange == default || d == endTimeRange),
+            It.Is<CancellationToken>(ct => token == CancellationToken.None || ct == token)));
+    }
+}
---
+++ UnitTests/MockedUnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using Nasurino.SmartWallet.Context.Repository.Contracts;
+
+namespace Nasurino.SmartWallet.UnitTests;
+
+/// <summary>
+/// Замоканый unit of work
+/// </summary>
+public class MockedUnitOfWork : IUnitOfWork
+{
+    private readonly Mock<ICashVaultRepository> mockedCashVaultRepository;
+    private readonly Mock<ISpendingAreaRepository> mockedSpendingAreaRepository;
+    private readonly Mock<ITransactionRepository> mockedTransactionRepository;
+    private readonly Mock<IUserRepository> mockedUserRepository;
+
+    ICashVaultRepository IUnitOfWork.CashVaultRepository => mockedCashVaultRepository.Object;
+
+    ISpendingAreaRepository IUnitOfWork.SpendingAreaRepository => mockedSpendingAreaRepository.Object;
+
+    ITransactionRepository IUnitOfWork.TransactionRepository => mockedTransactionRepository.Object;
+
+    IUserRepository IUnitOfWork.UserRepository => mockedUserRepository.Object;
+
+    /// <summary>
+    /// Инициализирует новый экземпляр <see cref="MockedUnitOfWork"/>
+    /// </summary>
+    public MockedUnitOfWork(Mock<ICashVaultRepository>? mockedCashVaultRepository = null,
+        Mock<ISpendingAreaRepository>? mockedSpendingAreaRepository = null,
+        Mock<ITransactionRepository>? mockedTransactionRepository = null,
+        Mock<IUserRepository>? mockedUserRepository = null)
+    {
+        this.mockedCashVaultRepository = mockedCashVaultRepository ?? new Mock<ICashVaultRepository>();
+        this.mockedSpendingAreaRepository = mockedSpendingAreaRepository ?? new Mock<ISpendingAreaRepository>();
+        this.mockedTransactionRepository =  mockedTransactionRepository ?? new Mock<ITransactionRepository>();
+        this.mockedUserRepository = mockedUserRepository ?? new Mock<IUserRepository>();
+    }
+
+    Task IUnitOfWork.SaveChangesAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
