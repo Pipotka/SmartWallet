102a600fec6bb5fa1056a6720dd728ed7252b7c8
Pipotka <157719919+Pipotka@users.noreply.github.com>
2026-05-03 23:48:17 +0300

    Добавил типы транзакций уменьщения и увеличения баланса категорий

--- Services/TransactionService.cs
+++ Services/TransactionService.cs
@@ -106,7 +106,7 @@ public sealed class TransactionService(IUnitOfWork unitOfWork,
 			
 			if (destinationAccount == null)
 			{
-				transaction.Type = TransactionType.AdjustmentDecrease;
+				transaction.Type = TransactionType.StorageDecreaseAdjustment;
 			}
 
 			if (destinationAccount is { IsStorage: true })
@@ -116,7 +116,7 @@ public sealed class TransactionService(IUnitOfWork unitOfWork,
 		}
 		else if (destinationAccount is { IsStorage: true })
 		{
-			transaction.Type = TransactionType.AdjustmentIncrease;
+			transaction.Type = TransactionType.StorageIncreaseAdjustment;
 		}
 		
 		_transactionRepository.Add(transaction);
