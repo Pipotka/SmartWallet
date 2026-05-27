5b34fd8a86bf39be71430a831330b23083265ac4
Pipotka <33nasrudin@gmail.com>
2026-05-27 18:48:34 +0300

    feat: add RefreshTokenRepository mock to MockedUnitOfWork

--- UnitTests.Services.Infrastructure/MockedUnitOfWork.cs
+++ UnitTests.Services.Infrastructure/MockedUnitOfWork.cs
@@ -11,6 +11,7 @@ public sealed class MockedUnitOfWork : IUnitOfWork
     private readonly Mock<ITransactionEndpointRepository> _mockedCashVaultRepository;
     private readonly Mock<ITransactionRepository> _mockedTransactionRepository;
     private readonly Mock<IUserRepository> _mockedUserRepository;
+    private readonly Mock<IRefreshTokenRepository> _mockedRefreshTokenRepository;
 
     ITransactionEndpointRepository IUnitOfWork.TransactionEndpointRepository => _mockedCashVaultRepository.Object;
 
@@ -18,16 +19,20 @@ public sealed class MockedUnitOfWork : IUnitOfWork
 
     IUserRepository IUnitOfWork.UserRepository => _mockedUserRepository.Object;
 
+    IRefreshTokenRepository IUnitOfWork.RefreshTokenRepository => _mockedRefreshTokenRepository.Object;
+
     /// <summary>
     /// Инициализирует новый экземпляр <see cref="MockedUnitOfWork"/>
     /// </summary>
     public MockedUnitOfWork(Mock<ITransactionEndpointRepository>? mockedCashVaultRepository = null,
         Mock<ITransactionRepository>? mockedTransactionRepository = null,
-        Mock<IUserRepository>? mockedUserRepository = null)
+        Mock<IUserRepository>? mockedUserRepository = null,
+        Mock<IRefreshTokenRepository>? mockedRefreshTokenRepository = null)
     {
         _mockedCashVaultRepository = mockedCashVaultRepository ?? new Mock<ITransactionEndpointRepository>();
         _mockedTransactionRepository =  mockedTransactionRepository ?? new Mock<ITransactionRepository>();
         _mockedUserRepository = mockedUserRepository ?? new Mock<IUserRepository>();
+        _mockedRefreshTokenRepository = mockedRefreshTokenRepository ?? new Mock<IRefreshTokenRepository>();
     }
 
     Task IUnitOfWork.SaveChangesAsync(CancellationToken cancellationToken) => Task.CompletedTask;
