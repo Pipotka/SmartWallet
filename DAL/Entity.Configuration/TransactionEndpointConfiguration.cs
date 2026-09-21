f4a6017e968859f5b2a147eaac34941e6ca3a82e
Pipotka <33nasrudin@gmail.com>
2026-09-21 15:08:26 +0300

    feat: configure EndpointType integer storage via EF value converter

--- DAL/Entity.Configuration/TransactionEndpointConfiguration.cs
+++ DAL/Entity.Configuration/TransactionEndpointConfiguration.cs
@@ -20,5 +20,8 @@ public class TransactionEndpointConfiguration : IEntityTypeConfiguration<Transac
 			.WithMany(x => x.TransactionEndpoints)
 			.HasForeignKey(x => x.UserId)
 			.OnDelete(DeleteBehavior.NoAction);
+
+		builder.Property(x => x.EndpointType)
+			.HasConversion<int>();
 	}
 }
